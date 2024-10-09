4a6f2f81eadc1eb21771c27cbbc559ddf90fe47b
Maobugichi <immansamuel846@gmail.com>
2024-10-09 01:40:43 -0700

    edit the success message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("form")
 const button = document.querySelector("button")
 let err = false
+let errMess = []
 const success = document.querySelector(".success")
 
 
@@ -8,18 +9,27 @@ form.addEventListener("submit", (e) => {
   e.preventDefault()
   const formData = new FormData(form)
   const formFields = ["fName", "lName", "email", "type", "message", "checkbox"]
+  for (const [key,value] of formData.entries()) {
+    if (value) {
+      errMess.push(`${key}:${value}`)
+    }
+    
+  } 
+
+  console.log(errMess)
   
   formFields.forEach(item => {
     const input = document.querySelector(`[name ="${item}"]`)
     const span = document.querySelector(`#err-${item}`)
-   
-    //console.log(!formData.get(item))
+    
+    
     if (!formData.get(item)) {
       input.classList.add("err-border")
       span.classList.add("block")
-    } else {
+      err = true
+      //console.log()
+    } else if (formData.get(item) && errMess.length === 6) {
       success.classList.add("animate")
-
       setTimeout(async () => {
         //success.classList.add("deanimate")
         success.classList.remove("animate")
@@ -30,6 +40,7 @@ form.addEventListener("submit", (e) => {
   })
     //const keys = Array.from(formData.keys)
    //console.log(err) 
+   errMess = []
     form.reset()
 })
 
